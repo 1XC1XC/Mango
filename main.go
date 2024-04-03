3d977cf40d30f5fe4f0374e2ce273923872c45cd
1XC1XC <1xct1xc@gmail.com>
2024-04-03 10:14:09 -0500

    0.1.0

--- main.go
+++ main.go
@@ -1,13 +1,6 @@
 package main
 
-import (
-    "fmt"
-    "os"
-)
-
 func main() {
-    fmt.Fprintf(os.Stdout, "\033[?25l")
-    defer fmt.Fprintf(os.Stdout, "\033[?25h")
     CLI()
     CleanMangoCache(MangoPath)
 }
