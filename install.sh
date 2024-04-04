edac38c2290036ee534f8269a2eb41968efb0b24
1XC1XC <1xct1xc@gmail.com>
2024-04-04 11:35:11 -0500

    0.1.0

--- install.sh
+++ install.sh
@@ -57,7 +57,9 @@ update_shell_config() {
         echo "PATH already includes $bin_dir in ~/${config_file##*/}"
     else
         while true; do
-            read -p "Do you want to add Mango to your PATH in ~/${config_file##*/}? [Y/n] " add_to_path
+            echo "Do you want to add Mango to your PATH in ~/${config_file##*/}? [Y/n] "
+            read -rsn1 add_to_path < "$(tty)"
+            echo
             case "$add_to_path" in
                 [Yy]* | "")
                     echo -e "\nexport PATH=\"$bin_dir:\$PATH\"" >> "$config_file"
